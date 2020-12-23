d575bc1e996a0055c343093e5a8074149748fb0c
Aleksandr Kushniruk <kushniruk_aleksandr@ukr.net>
2020-12-23 21:22:09 +0200

    Updates list sort component: adds data attributes to template and methods to handle click event

--- src/view/list-sort.js
+++ src/view/list-sort.js
@@ -1,15 +1,35 @@
 import AbstractView from "./abstract";
+import {SortTypes} from "../constants";
 
 const createListSortTemplate = () => {
   return `<ul class="sort">
-    <li><a href="#" class="sort__button">Sort by default</a></li>
-    <li><a href="#" class="sort__button">Sort by date</a></li>
-    <li><a href="#" class="sort__button sort__button--active">Sort by rating</a></li>
+    <li><a href="#" class="sort__button" data-sort-type="${SortTypes.DEFAULT}">Sort by default</a></li>
+    <li><a href="#" class="sort__button" data-sort-type="${SortTypes.DATE}">Sort by date</a></li>
+    <li><a href="#" class="sort__button sort__button--active" data-sort-type="${SortTypes.RATING}">Sort by rating</a></li>
   </ul>`;
 };
 
 export default class ListSort extends AbstractView {
+  constructor() {
+    super();
+    this._sortTypeChangeHandler = this._sortTypeChangeHandler.bind(this);
+  }
+
   getTemplate() {
     return createListSortTemplate();
+  }
+
+  _sortTypeChangeHandler(evt) {
+    if (evt.target.tagName !== `A`) {
+      return;
+    }
+
+    evt.preventDefault();
+    this._callback.sortTypeChange(evt.target.dataset.sortType);
+  }
+
+  setSortTypeChangeClickHandler(callback) {
+    this._callback.sortTypeChange = callback;
+    this.getElement().addEventListener(`click`, this._sortTypeChangeHandler);
   }
 }

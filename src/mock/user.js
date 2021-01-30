140b28704dde9cfd90f66bee6a6136a1de807935
Aleksandr Kushniruk <kushniruk_aleksandr@ukr.net>
2021-01-30 15:16:11 +0200

    Removes mocks

--- src/mock/user.js
+++
@@ -1,9 +0,0 @@
-import {getRandomInteger} from "./helpers";
-
-export const generateUser = () => {
-  return {
-    watchList: new Array(getRandomInteger(0, 25)),
-    history: new Array(getRandomInteger(0, 25)),
-    favorites: new Array(getRandomInteger(0, 25)),
-  };
-};
